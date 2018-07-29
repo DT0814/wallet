35192d056edfc40361bfdbca9e5fce8c71cde369
DT0814 <729742011@qq.com>
2018-07-29 13:35:49 +0800

    0.000000004

--- app/src/main/java/lr/com/wallet/activity/TxActivity.java
+++ app/src/main/java/lr/com/wallet/activity/TxActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
@@ -34,8 +36,11 @@ import java.util.regex.Pattern;
 
 import lr.com.wallet.R;
 import lr.com.wallet.dao.WalletDao;
+import lr.com.wallet.pojo.CoinPojo;
 import lr.com.wallet.pojo.ETHWallet;
+import lr.com.wallet.utils.CoinUtils;
 import lr.com.wallet.utils.ETHWalletUtils;
+import lr.com.wallet.utils.JsonUtils;
 import lr.com.wallet.utils.Md5Utils;
 import lr.com.wallet.utils.Web3jUtil;
 
@@ -44,12 +49,11 @@ import lr.com.wallet.utils.Web3jUtil;
  * Created by dt0814 on 2018/7/16.
  */
 
-public class TxActivity extends Activity {
+public class TxActivity extends Activity implements View.OnClickListener {
     private EditText toAddress;
     private EditText TNum;
     private TextView txPrice;
     private Button TranscationBut;
-    private Context context;
     private View orderView;
     private ETHWallet wallet;
     private BigInteger gase = new BigInteger("21000");
@@ -60,9 +64,7 @@ public class TxActivity extends Activity {
     private String costNum;
     private ImageButton saoyisao;
     private ImageButton trxPreBut;
-    private AlertDialog alertDialog;
-    private AlertDialog mDialog;
-    private static final int NOT_NOTICE = 2;//如果勾选了不再询问
+    CoinPojo coin;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -72,76 +74,34 @@ public class TxActivity extends Activity {
         toAddress = findViewById(R.id.toAddress);
         TNum = findViewById(R.id.TNum);
         TranscationBut = findViewById(R.id.TranscationBut);
-        context = getBaseContext();
         txPrice = findViewById(R.id.txPrice);
-        seekBar = findViewById(R.id.seekBar);
-        seekBar.setMax(10000);
-        seekBar.setMin(1);
+
         inflater = getLayoutInflater();
         trxPreBut = findViewById(R.id.trxPreBut);
+        trxPreBut.setOnClickListener(this);
         saoyisao = findViewById(R.id.saoyisao);
-        saoyisao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (ContextCompat.checkSelfPermission(TxActivity.this,
-                        android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    //先判断有没有权限 ，没有就在这里进行权限的申请
-                    ActivityCompat.requestPermissions(TxActivity.this,
-                            new String[]{android.Manifest.permission.CAMERA}, 1);
-                } else {
-                    startActivityForResult(new Intent(TxActivity.this, CaptureActivity.class), 0);
-                }
+        saoyisao.setOnClickListener(this);
+        Intent intent = getIntent();
+        String coinJson = intent.getStringExtra("coin");
+        coin = JsonUtils.jsonToPojo(coinJson, CoinPojo.class);
 
-            }
-        });
+        initSeekBar();
 
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                System.out.println("onProgressChanged");
-                int progress = seekBar.getProgress();
-                BigDecimal bigDecimal = Convert.toWei(progress / 100 + "", Convert.Unit.GWEI);
-                BigDecimal bigDecimal1 = new BigDecimal(gase.toString());
-                gasPrice = bigDecimal.toBigInteger();
-                bigDecimal = bigDecimal.multiply(bigDecimal1);
-                BigDecimal eth = Convert.fromWei(bigDecimal, Convert.Unit.ETHER);
-                costNum = eth.toString();
-                txPrice.setText(costNum + "\b\beth");
-                System.out.println(progress + "___________" + gasPrice);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                System.out.println("onStartTrackingTouch");
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                System.out.println("onStopTrackingTouch");
-            }
-        });
-
-        trxPreBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TxActivity.this.finish();
-            }
-        });
         TranscationBut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (toAddress.getText().toString().trim().equals("")) {
+                String to = toAddress.getText().toString().trim();
+                if (to.equals("") || to.length() != 42) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(TxActivity.this);
                     builder.setTitle("提示");
-                    builder.setMessage("转账地址不呢为空");
+                    builder.setMessage("转账地址为0x开头的42位字符串");
                     builder.setPositiveButton("是", null);
                     builder.show();
+                    return;
                 }
-                String content = TNum.getText().toString().trim();
-
+                String num = TNum.getText().toString().trim();
                 String pattern = "[0-9]+([.]{1}[0-9]+){0,1}";
-
-                boolean isMatch = Pattern.matches(pattern, content);
+                boolean isMatch = Pattern.matches(pattern, num);
                 AlertDialog.Builder alertbBuilder = new AlertDialog.Builder(TxActivity.this);
                 if (!isMatch) {
                     alertbBuilder.setTitle("提示");
@@ -150,17 +110,19 @@ public class TxActivity extends Activity {
                     alertbBuilder.show();
                     return;
                 }
-
                 alertbBuilder = new AlertDialog.Builder(TxActivity.this);
                 orderView = inflater.inflate(R.layout.order_layout, null);
                 TextView toAddressMassage = orderView.findViewById(R.id.toAddressMassage);
+
                 TextView payAddressMassage = orderView.findViewById(R.id.payAddressMassage);
                 TextView costMassage = orderView.findViewById(R.id.costMassage);
                 TextView numMassage = orderView.findViewById(R.id.numMassage);
                 toAddressMassage.setText(toAddress.getText());
                 payAddressMassage.setText(wallet.getAddress());
-                costMassage.setText(costNum);
-                numMassage.setText(TNum.getText() + "\b\beth");
+
+                costMassage.setText(calculationCostNum(gase, gasPrice));
+
+                numMassage.setText(num + "\b\b" + coin.getCoinSymbolName());
                 alertbBuilder.setView(orderView);
                 alertbBuilder.setTitle("支付详情").setMessage("").setPositiveButton("确定",
                         new DialogInterface.OnClickListener() {
@@ -180,10 +142,48 @@ public class TxActivity extends Activity {
                                             return;
                                         }
                                         try {
-                                            System.out.println(gasPrice + "___________" + gase);
-                                            Web3jUtil.ethTransaction(wallet.getAddress(), toAddress.getText().toString(),
-                                                    ETHWalletUtils.derivePrivateKey(wallet, pwd), gasPrice, gase,
-                                                    TNum.getText().toString());
+                                            if (null == gase) {
+                                                gase = new BigInteger("21000");
+                                            }
+                                            if (null == gasPrice) {
+                                                gasPrice = new BigInteger("1000000000");
+                                            }
+                                            String tNum = TNum.getText().toString();
+                                            if (coin.getCoinSymbolName().equalsIgnoreCase("ETH")) {
+                                                double walletNum = wallet.getNum().doubleValue();
+                                                if (walletNum < Double.parseDouble(num) || wallet.getNum().doubleValue() <= 0) {
+                                                    Toast.makeText(TxActivity.this, "账户余额不足", Toast.LENGTH_SHORT).show();
+                                                    return;
+                                                }
+                                                Web3jUtil.ethTransaction(wallet.getAddress(),
+                                                        to,
+                                                        ETHWalletUtils.derivePrivateKey(wallet, pwd),
+                                                        gasPrice,
+                                                        gase,
+                                                        TNum.getText().toString());
+                                            } else {
+                                                double walletNum = wallet.getNum().doubleValue();
+                                                if (wallet.getNum().doubleValue() <= 0) {
+                                                    Toast.makeText(TxActivity.this, "ETH余额不足", Toast.LENGTH_SHORT).show();
+                                                    return;
+                                                }
+                                                if (Double.parseDouble(coin.getCoinCount()) < Double.parseDouble(num)) {
+                                                    Toast.makeText(TxActivity.this, coin.getCoinSymbolName() + "余额不足", Toast.LENGTH_SHORT).show();
+                                                    return;
+                                                }
+                                                new Thread(new Runnable() {
+                                                    @Override
+                                                    public void run() {
+                                                        CoinUtils.transaction(wallet.getAddress(),
+                                                                to,
+                                                                coin.getCoinAddress(),
+                                                                gasPrice, gase,
+                                                                ETHWalletUtils.derivePrivateKey(wallet, pwd),
+                                                                tNum);
+
+                                                    }
+                                                }).start();
+                                            }
                                             Toast.makeText(TxActivity.this, "成功发起转账请耐心等待", Toast.LENGTH_LONG).show();
                                             startActivity(new Intent(TxActivity.this, MainFragmentActivity.class));
                                         } catch (IOException e) {
@@ -229,10 +229,17 @@ public class TxActivity extends Activity {
                                 wallet = WalletDao.getCurrentWallet();
                                 gase = Web3jUtil.getEstimateGas(wallet.getAddress(), toAddress.getText().toString().trim());
                                 gasPrice = Web3jUtil.getGasPrice();
-                                BigDecimal bigDecimal = Convert.fromWei(gasPrice.toString(), Convert.Unit.GWEI);
-                                BigInteger bigInteger = bigDecimal.toBigInteger();
-                                Integer integer = new Integer(bigInteger.toString());
-                                seekBar.setProgress(integer * 100);
+                                if (gase != null) {
+                                    seekBar.setProgress(Integer.parseInt(gase.toString()));
+                                } else {
+                                    seekBar.setProgress(21000);
+                                }
+                                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        txPrice.setText(calculationCostNum(gase, gasPrice) + "\b\beth");
+                                    }
+                                });
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
@@ -244,6 +251,42 @@ public class TxActivity extends Activity {
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void initSeekBar() {
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(121000);
+        seekBar.setMin(21000);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int progress = seekBar.getProgress();
+                gase = new BigInteger(progress + "");
+                txPrice.setText(calculationCostNum(gase, gasPrice) + "\b\beth");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
+    private String calculationCostNum(BigInteger gase, BigInteger gasePrice) {
+        if (null == gase) {
+            gase = new BigInteger("21000");
+        }
+        if (null == gasePrice) {
+            gasePrice = new BigInteger("1000000000");
+        }
+        BigDecimal bigDecimal1 = new BigDecimal(gase.toString());
+        BigDecimal bigDecimal = new BigDecimal(gasePrice);
+        bigDecimal = bigDecimal.multiply(bigDecimal1);
+        BigDecimal eth = Convert.fromWei(bigDecimal, Convert.Unit.ETHER);
+        return eth.toString();
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -271,5 +314,24 @@ public class TxActivity extends Activity {
                 break;
         }
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.saoyisao:
+                if (ContextCompat.checkSelfPermission(TxActivity.this,
+                        android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    //先判断有没有权限 ，没有就在这里进行权限的申请
+                    ActivityCompat.requestPermissions(TxActivity.this,
+                            new String[]{android.Manifest.permission.CAMERA}, 1);
+                } else {
+                    startActivityForResult(new Intent(TxActivity.this, CaptureActivity.class), 0);
+                }
+                break;
+            case R.id.trxPreBut:
+                TxActivity.this.finish();
+                break;
+        }
     }
 }
